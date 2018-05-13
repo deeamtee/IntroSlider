9dcd804a6e1ab813417f4294927bb0f04441f57a
Sergey Konstantinov <iceforcy@gmail.com>
2018-05-13 16:43:02 +0300

    Initial commit

---
+++ app/src/main/java/info/androidhive/introslider/NotMainActivity.java
@@ -0,0 +1,33 @@
+package info.androidhive.introslider;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class NotMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_not);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        findViewById(R.id.btn_play_again).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // We normally won't show the welcome slider again in real app
+                // but this is for testing
+                PrefManager prefManager = new PrefManager(getApplicationContext());
+
+                // make first time launch TRUE
+                prefManager.setFirstTimeLaunch(true);
+
+                startActivity(new Intent(NotMainActivity.this, WelcomeActivity.class));
+                finish();
+            }
+        });
+    }
+}
