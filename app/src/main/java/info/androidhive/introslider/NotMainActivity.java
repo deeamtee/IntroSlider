fa89c0e5fd600c39f39a0d11cc07f9377503beb3
Sergey Konstantinov <iceforcy@gmail.com>
2018-05-14 10:36:53 +0300

    + MovieActivity

--- app/src/main/java/info/androidhive/introslider/NotMainActivity.java
+++ app/src/main/java/info/androidhive/introslider/NotMainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import info.androidhive.introslider.Manager.PrefManager;
+
 public class NotMainActivity extends AppCompatActivity {
 
     @Override
